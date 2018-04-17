60cfef965165825afc90d22ced5e20df9002d710
U.Nakamura <usa@garbagecollect.jp>
2018-04-17 10:12:51 +0900

    Initial Commit

--- WindowsFormsApp1/FormMain.Designer.cs
+++ WindowsFormsApp1/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormMain";
+            this.Text = "ImageReciever";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
