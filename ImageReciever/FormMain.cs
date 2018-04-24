ce46e79c3a210a698b18c062c0a34e9d70bcf244
U.Nakamura <usa@garbagecollect.jp>
2018-04-24 11:50:35 +0900

    Remove unused usings

--- ImageReciever/FormMain.cs
+++ ImageReciever/FormMain.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ImageReciever
